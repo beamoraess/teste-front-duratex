d83963f31db0f90f7ba448f25a2727cf2e01895e
beamoraess <beatrizm.silva45@gmail.com>
2019-09-09 15:43:26 -0300

    Updates

---
+++ precache-manifest.c63e006e42ffba6f305906599f1cfd2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7739acd5329a22a59f61b1a05b28d06",
+    "url": "/teste-front-duratex/index.html"
+  },
+  {
+    "revision": "88d90e55afe1839fa253",
+    "url": "/teste-front-duratex/static/css/main.7c0680f3.chunk.css"
+  },
+  {
+    "revision": "b1014f2258a09d6fd496",
+    "url": "/teste-front-duratex/static/js/2.1d558f8f.chunk.js"
+  },
+  {
+    "revision": "88d90e55afe1839fa253",
+    "url": "/teste-front-duratex/static/js/main.34df9d01.chunk.js"
+  },
+  {
+    "revision": "7619a45a095be6d9c21e",
+    "url": "/teste-front-duratex/static/js/runtime~main.d3d99b81.js"
+  }
+]);
